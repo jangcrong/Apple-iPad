0e829310b5c3c4f3a899852d0985d96135df2b2b
jangcrong <jangcrong19@gmail.com>
2023-11-17 21:02:04 +0900

    프로젝트 시작

---
+++ js/main.js
@@ -0,0 +1,165 @@
+import ipads from '../data/ipads.js'
+import navigations from '../data/navigations.js'
+
+// 장바구니!
+const basketStarterEl = document.querySelector('header .basket-starter')
+const basketEl = basketStarterEl.querySelector('.basket')
+// document는 html 전제에서 header .basket을 찾기 때문에 딜레이가 걸린다.
+/*이미 찾은 basketStarterEl에서 찾으면 조금이라도 더 빨리 찾을 수 있다. */
+
+basketStarterEl.addEventListener('click', function (event) {
+  event.stopPropagation()
+  if (basketEl.classList.contains('show')) { //contains메소드는 show라는 클레스를 있는지 없는지 확인해서 true, fals값을 반환한다.
+    hideBasket()
+    // hide
+  } else {
+    showBasket()
+    // show
+  }
+})
+basketEl.addEventListener('click', function (event) {
+  event.stopPropagation()
+})
+
+window.addEventListener('click', function () {
+  hideBasket()
+})
+
+function showBasket() { // 장바구니 보여주기
+  basketEl.classList.add('show')
+}
+function hideBasket() { // 장바구니 안보이기
+  basketEl.classList.remove('show')
+}
+
+
+// 검색!
+const headerEl = document.querySelector('header')
+const headerMenuEls = [...headerEl.querySelectorAll('ul.menu > li')] //querySelectorAll li테그 전부다 선택한다.
+const searchWrapEl = headerEl.querySelector('.search-wrap')
+const searchStarterEl = headerEl.querySelector('.search-starter')
+const searchCloserEl = searchWrapEl.querySelector('.search-closer')
+const searchShadowEl = searchWrapEl.querySelector('.shadow')
+const searchInputEl = searchWrapEl.querySelector('input')
+const searchDelayEls = [...searchWrapEl.querySelectorAll('li')] /* querySelectorAll은 forEach메소드만 사용이 가능하기 때문에 배열데이터로 바꿔야 한다. */
+
+searchStarterEl.addEventListener('click', showSearch)
+searchCloserEl.addEventListener('click', hideSearch)
+searchShadowEl.addEventListener('click', hideSearch)
+
+function showSearch() {
+  headerEl.classList.add('searching')
+  document.documentElement.classList.add('fixed')
+  headerMenuEls.reverse().forEach(function (el, index) {
+    el.style.transitionDelay = index * .4 / headerMenuEls.length + 's' // 메뉴 개수 12개를 입력하면 나중에 메뉴 개수가 달라지게 되면 코드도 수정해야 하기 때문에 자동화를 시킨다.
+  })
+  searchDelayEls.forEach(function (el, index) {
+    el.style.transitionDelay = index * .4 / searchDelayEls.length + 's'
+  })
+  setTimeout(function () { // setTimeout 몇초 뒤에 동작한다. 600 은 0.6s를 의미함.
+    searchInputEl.focus()
+  }, 600)
+}
+function hideSearch() {
+  headerEl.classList.remove('searching')
+  document.documentElement.classList.remove('fixed')
+  headerMenuEls.reverse().forEach(function (el, index) {
+    el.style.transitionDelay = index * .4 / headerMenuEls.length + 's'
+  })
+  searchDelayEls.reverse().forEach(function (el, index) {
+    el.style.transitionDelay = index * .4 / searchDelayEls.length + 's'
+  })
+  searchDelayEls.reverse()
+  searchInputEl.value = '' // 검색바에 검색을 하고 나서 검색창이 닫히면 검색했던 단어를 초기화 한다.
+}
+
+
+// 요소의 가시성 관찰
+const io = new IntersectionObserver(function (entries) {
+  entries.forEach(function (entry) {
+    if (!entry.isIntersecting) {
+      return
+    }
+    entry.target.classList.add('show')
+  })
+})
+const infoEls = document.querySelectorAll('.info')
+infoEls.forEach(function (el) {
+  io.observe(el)
+})
+
+
+// 비디오 재생!
+const video = document.querySelector('.stage video')
+const playBtn = document.querySelector('.stage .controller--play')
+const pauseBtn = document.querySelector('.stage .controller--pause')
+
+playBtn.addEventListener('click', function () {
+  video.play()
+  playBtn.classList.add('hide')
+  pauseBtn.classList.remove('hide')
+})
+pauseBtn.addEventListener('click', function () {
+  video.pause()
+  playBtn.classList.remove('hide')
+  pauseBtn.classList.add('hide')
+})
+
+
+// '당신에게 맞는 iPad는?' 랜더링!
+const itemsEl = document.querySelector('section.compare .items')
+ipads.forEach(function (ipad) {
+  const itemEl = document.createElement('div')
+  itemEl.classList.add('item')
+
+  let colorList = ''
+  ipad.colors.forEach(function (color) {
+    colorList += `<li style="background-color: ${color};"></li>`
+  })
+  
+  itemEl.innerHTML = /* html */ `
+    <div class="thumbnail">
+      <img src="${ipad.thumbnail}" alt="${ipad.name}" />
+    </div>
+    <ul class="colors">
+      ${colorList}
+    </ul>
+    <h3 class="name">${ipad.name}</h3>
+    <p class="tagline">${ipad.tagline}</p>
+    <p class="price">₩${ipad.price.toLocaleString('en-US')}부터</p>
+    <button class="btn">구입하기</button>
+    <a href="${ipad.url}" class="link">더 알아보기</a>
+  `
+
+  itemsEl.append(itemEl)
+})
+
+
+const navigationsEl = document.querySelector('footer .navigations')
+navigations.forEach(function (nav) {
+  const mapEl = document.createElement('div')
+  mapEl.classList.add('map')
+
+  let mapList = ''
+  nav.maps.forEach(function (map) {
+    mapList += /* html */ `<li>
+      <a href="${map.url}">${map.name}</a>
+    </li>`
+  })
+
+  mapEl.innerHTML = /* html */ `
+  <h3>
+    <span class="text">${nav.title}</span>
+  </h3>
+  <ul>
+    ${mapList}
+  </ul>
+  `
+
+  navigationsEl.append(mapEl)
+})
+
+
+const thisYearEl = document.querySelector('span.this-year')
+thisYearEl.textContent = new Date().getFullYear()
+//new Date는 html의 class같은 녀석이다. 생성자 함수
